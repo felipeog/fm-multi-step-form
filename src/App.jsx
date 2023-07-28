2ecc03f0f649c49b6a484d71e97361e6ef582a33
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-28 20:21:14 -0300

    Create Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "@legendapp/state/react";
 
 import { store$ } from "./store";
+import { Button } from "./components/Button";
 
 function App() {
   const fullname = useSelector(
@@ -11,16 +12,21 @@ function App() {
     <>
       <h1>{fullname}</h1>
 
-      <div className="h-20 w-20 bg-primary-100"></div>
-      <div className="h-20 w-20 bg-primary-200"></div>
-      <div className="h-20 w-20 bg-primary-300"></div>
-      <div className="h-20 w-20 bg-primary-400"></div>
-      <div className="h-20 w-20 bg-secondary"></div>
-      <div className="h-20 w-20 bg-neutral-100"></div>
-      <div className="h-20 w-20 bg-neutral-200"></div>
-      <div className="h-20 w-20 bg-neutral-300"></div>
-      <div className="h-20 w-20 bg-neutral-400"></div>
-      <div className="h-20 w-20 bg-neutral-500"></div>
+      <Button variant="solid" scheme="regular">
+        solid regular
+      </Button>
+
+      <Button variant="solid" scheme="highlight">
+        solid highlight
+      </Button>
+
+      <Button variant="link" scheme="regular">
+        link regular
+      </Button>
+
+      <Button variant="link" scheme="highlight">
+        link highlight
+      </Button>
     </>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,51 @@
+import { tv } from "tailwind-variants";
+
+const button = tv({
+  variants: {
+    variant: {
+      solid:
+        "rounded-md px-4 py-2 text-neutral-100 hover:opacity-80 active:opacity-90",
+      link: "py-2 hover:text-primary-400 active:opacity-80",
+    },
+    scheme: {
+      regular: "",
+      highlight: "",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: "solid",
+      scheme: "regular",
+      className: "bg-primary-400",
+    },
+    {
+      variant: "solid",
+      scheme: "highlight",
+      className: "bg-primary-300",
+    },
+    {
+      variant: "link",
+      scheme: "regular",
+      className: "text-neutral-500",
+    },
+    {
+      variant: "link",
+      scheme: "highlight",
+      className: "text-primary-300",
+    },
+  ],
+  defaultVariants: {
+    variant: "solid",
+    scheme: "regular",
+  },
+});
+
+export function Button({ children, variant, scheme, ...props }) {
+  console.log(button({ variant, scheme }));
+
+  return (
+    <button className={button({ variant, scheme })} {...props}>
+      {children}
+    </button>
+  );
+}
