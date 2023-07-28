553402b5dc5bc322bf5f951158e3b3ce547a5356
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-28 19:12:33 -0300

    Set theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,22 @@ function App() {
     () => `${store$.fname.get()} ${store$.lname.get()}`,
   );
 
-  return <h1>{fullname}</h1>;
+  return (
+    <>
+      <h1>{fullname}</h1>
+
+      <div className="h-20 w-20 bg-primary-100"></div>
+      <div className="h-20 w-20 bg-primary-200"></div>
+      <div className="h-20 w-20 bg-primary-300"></div>
+      <div className="h-20 w-20 bg-primary-400"></div>
+      <div className="h-20 w-20 bg-secondary"></div>
+      <div className="h-20 w-20 bg-neutral-100"></div>
+      <div className="h-20 w-20 bg-neutral-200"></div>
+      <div className="h-20 w-20 bg-neutral-300"></div>
+      <div className="h-20 w-20 bg-neutral-400"></div>
+      <div className="h-20 w-20 bg-neutral-500"></div>
+    </>
+  );
 }
 
 export default App;
