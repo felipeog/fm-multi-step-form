6a6eb26f3dfb5c38ebe39d4ee4805854b94710b2
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-28 21:53:20 -0300

    Create constants

---
+++ src/constants.js
@@ -0,0 +1,46 @@
+export const PLANS = [
+  {
+    id: "arcade",
+    name: "Arcade",
+    monthly: "$9/mo",
+    yearly: "$90/yr",
+  },
+  {
+    id: "advanced",
+    name: "Advanced",
+    monthly: "$12/mo",
+    yearly: "$120/yr",
+  },
+  {
+    id: "pro",
+    name: "Pro",
+    monthly: "$15/mo",
+    yearly: "$150/yr",
+  },
+];
+
+export const ADDONS = [
+  {
+    id: "online-service",
+    name: "Online service",
+    monthly: "$1/mo",
+    yearly: "$10/yr",
+    description: "Access to multiplayer games",
+  },
+  {
+    id: "larger-storage",
+    name: "Larger storage",
+    monthly: "$2/mo",
+    yearly: "$20/yr",
+    description: "Extra 1TB of cloud save",
+  },
+  {
+    id: "customizable-profile",
+    name: "Customizable profile",
+    monthly: "$2/mo",
+    yearly: "$20/yr",
+    description: "Custom theme on your profile",
+  },
+];
+
+export const RECURRENCE = ["monthly", "yearly"];
