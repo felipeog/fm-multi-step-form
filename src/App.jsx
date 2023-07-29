ecdeae986e279a567061668ea0cbe837f044a136
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-28 21:55:24 -0300

    Print store

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useSelector } from "@legendapp/state/react";
-
 import { store$ } from "./store";
-import { Header } from "./components/Header";
 
 function App() {
-  const fullname = useSelector(
-    () => `${store$.fname.get()} ${store$.lname.get()}`,
-  );
-
   return (
     <main className="min-h-screen bg-primary-100">
-      <h1>{fullname}</h1>
-
-      <Header title="Personal info" subtitle="Please, provide your name" />
-      <Header
-        title="Thank you!"
-        subtitle="If you need support, email us"
-        className="text-center"
-      />
+      <pre>{JSON.stringify(store$.get(), null, 2)}</pre>
     </main>
   );
 }
