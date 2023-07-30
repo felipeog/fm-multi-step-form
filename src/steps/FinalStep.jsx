ae1b6d5024a4358c5e990288faefa34721bf28dc
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-30 18:17:00 -0300

    Complete FinalStep

--- src/steps/FinalStep.jsx
+++ src/steps/FinalStep.jsx
@@ -1,5 +1,15 @@
 import { Header } from "../components/Header";
+import thankYou from "../assets/icon-thank-you.svg";
 
 export function FinalStep() {
-  return <Header title="FinalStep" subtitle="FinalStep" />;
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-8 text-center">
+      <img src={thankYou} alt="" />
+
+      <Header
+        title="Thank you!"
+        subtitle="Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com."
+      />
+    </div>
+  );
 }
