d93c6e017b5ca1024f9aabaf553ebc54574fd5b7
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-30 18:12:45 -0300

    Add description to recurrence constant

--- src/constants/recurrences.js
+++ src/constants/recurrences.js
@@ -2,11 +2,13 @@ export const RECURRENCES = {
   monthly: {
     id: "monthly",
     name: "Monthly",
+    description: "per month",
     shorthand: "mo",
   },
   yearly: {
     id: "yearly",
     name: "Yearly",
+    description: "per year",
     shorthand: "yr",
   },
 };
