c91e16a81589f9829d233338465165d269b129c8
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-30 18:45:02 -0300

    Prevent layout from breaking on smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Steps } from "./components/Steps";
 
 export const App = () => {
   return (
-    <div className="grid h-screen w-screen place-content-center bg-neutral-300 p-4">
+    <div className="grid h-screen min-h-[600px] w-screen min-w-[900px] place-content-center bg-neutral-300 p-4">
       <main className="flex rounded-2xl bg-neutral-100 p-4 shadow-lg">
         <Sidebar />
         <Steps />
