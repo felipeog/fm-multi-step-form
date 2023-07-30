6bccae78dce18edffb7f3ce22b748f8f9684a5e7
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-30 12:19:08 -0300

    Create Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import { tv } from "tailwind-variants";
+
+const card = tv({
+  base: "rounded-lg border border-neutral-400 transition-all cursor-pointer hover:border-primary-400 hover:opacity-90 active:opacity-80",
+  variants: {
+    active: {
+      true: "border-primary-400 bg-neutral-200",
+    },
+  },
+});
+
+export function Card({ children, className, active, onClick }) {
+  return (
+    <button
+      className={card({ active: Boolean(active), className })}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
