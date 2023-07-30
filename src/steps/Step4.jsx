b0f2324ddc8d06396e4f23589f1c6b3784fe038f
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-30 18:13:07 -0300

    Complete Step4

--- src/steps/Step4.jsx
+++ src/steps/Step4.jsx
@@ -1,5 +1,73 @@
+import { tv } from "tailwind-variants";
+
 import { Header } from "../components/Header";
+import { formatPrice } from "../helpers/formatPrice";
+import { store$ } from "../store";
+
+const table = tv({
+  slots: {
+    row: "text-primary-400 flex justify-between items-center",
+    key: "text-sm text-neutral-500",
+    value: "text-sm text-primary-400",
+  },
+});
+const slots = table();
 
 export function Step4() {
-  return <Header title="Step4" subtitle="Step4" />;
+  const selectedPlan = store$.plan.use();
+  const selectedAddons = store$.addons.use();
+  const selectedRecurrence = store$.recurrence.use();
+  const priceKey = selectedRecurrence.id;
+  const hasAddons = Boolean(selectedAddons.length);
+
+  function getTotal() {
+    const planPrice = selectedPlan[priceKey];
+    const addonsPrice = selectedAddons.reduce(
+      (acc, cur) => acc + cur[priceKey],
+      0,
+    );
+
+    return formatPrice(planPrice + addonsPrice);
+  }
+
+  return (
+    <div>
+      <Header
+        title="Finishing up"
+        subtitle="Double-check everything looks OK before confirming."
+      />
+
+      <div className="rounded-lg bg-neutral-200 p-4 text-left">
+        <div className={slots.row()}>
+          <p className="text-primary-400">
+            {selectedPlan.name} ({selectedRecurrence.name})
+          </p>
+          <p className="font-bold text-primary-400">
+            {formatPrice(selectedPlan[priceKey])}/{selectedRecurrence.shorthand}
+          </p>
+        </div>
+
+        {hasAddons && (
+          <ul className="mt-4 flex flex-col gap-2 border-t border-t-neutral-400 pt-4">
+            {selectedAddons.map((selectedAddon) => (
+              <li className={slots.row()} key={selectedAddon.id}>
+                <p className={slots.key()}>{selectedAddon.name}</p>
+                <p className={slots.value()}>
+                  +{formatPrice(selectedAddon[priceKey])}/
+                  {selectedRecurrence.shorthand}
+                </p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div className="flex items-center justify-between p-4 text-primary-400">
+        <p className={slots.key()}>Total ({selectedRecurrence.description})</p>
+        <p className="text-lg font-bold text-primary-300">
+          {getTotal()}/{selectedRecurrence.shorthand}
+        </p>
+      </div>
+    </div>
+  );
 }
