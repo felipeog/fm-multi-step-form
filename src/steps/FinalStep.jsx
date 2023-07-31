f2b2e2462aa4ff8516504500938b29dfe60a9815
Felipe OG <felipeog476@gmail.com>
2023-07-31 18:22:48 +0000

    Use SVG as React component

--- src/steps/FinalStep.jsx
+++ src/steps/FinalStep.jsx
@@ -1,10 +1,10 @@
 import { Header } from "../components/Header";
-import thankYou from "../assets/icon-thank-you.svg";
+import { ReactComponent as IconThankYou } from "../assets/icon-thank-you.svg";
 
 export function FinalStep() {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-8 text-center">
-      <img src={thankYou} alt="" />
+      <IconThankYou />
 
       <Header
         title="Thank you!"
--- src/steps/Step2.jsx
+++ src/steps/Step2.jsx
@@ -5,16 +5,16 @@ import { Header } from "../components/Header";
 import { PLANS_LIST } from "../constants/plans";
 import { RECURRENCES } from "../constants/recurrences";
 import { store$ } from "../store";
-import advanced from "../assets/icon-advanced.svg";
-import arcade from "../assets/icon-arcade.svg";
-import pro from "../assets/icon-pro.svg";
+import { ReactComponent as IconAdvanced } from "../assets/icon-advanced.svg";
+import { ReactComponent as IconArcade } from "../assets/icon-arcade.svg";
+import { ReactComponent as IconPro } from "../assets/icon-pro.svg";
 import { priceFormatter } from "../helpers/priceFormatter";
 import { Toggle } from "../components/Toggle";
 
 const icons = {
-  advanced,
-  arcade,
-  pro,
+  advanced: IconAdvanced,
+  arcade: IconArcade,
+  pro: IconPro,
 };
 const variants = {
   initial: {
@@ -43,38 +43,45 @@ export function Step2() {
       />
 
       <ul className="grid grid-cols-3 gap-4">
-        {PLANS_LIST.map((plan) => (
-          <li key={plan.id}>
-            <Card
-              onClick={() => store$.plan.set(plan)}
-              active={plan.id === selectedPlan?.id}
-              className="w-full p-4 text-left"
-            >
-              <img src={icons[plan.id]} alt={plan.name} />
+        {PLANS_LIST.map((plan) => {
+          const Icon = icons[plan.id];
 
-              <p className="mt-10 text-primary-400">{plan.name}</p>
+          return (
+            <li key={plan.id}>
+              <Card
+                onClick={() => store$.plan.set(plan)}
+                active={plan.id === selectedPlan?.id}
+                className="w-full p-4 text-left"
+              >
+                <Icon />
 
-              <p className="text-sm text-neutral-500">
-                {priceFormatter.formatWithRecurrence(plan, selectedRecurrence)}
-              </p>
+                <p className="mt-10 text-primary-400">{plan.name}</p>
 
-              <AnimatePresence>
-                {selectedRecurrence.id === RECURRENCES.yearly.id && (
-                  <motion.p
-                    className="text-xs text-primary-400"
-                    variants={variants}
-                    initial="initial"
-                    animate="enter"
-                    exit="initial"
-                  >
-                    {plan.yearlyFreeMonths} month
-                    {plan.yearlyFreeMonths === 1 ? "" : "s"} free
-                  </motion.p>
-                )}
-              </AnimatePresence>
-            </Card>
-          </li>
-        ))}
+                <p className="text-sm text-neutral-500">
+                  {priceFormatter.formatWithRecurrence(
+                    plan,
+                    selectedRecurrence,
+                  )}
+                </p>
+
+                <AnimatePresence>
+                  {selectedRecurrence.id === RECURRENCES.yearly.id && (
+                    <motion.p
+                      className="text-xs text-primary-400"
+                      variants={variants}
+                      initial="initial"
+                      animate="enter"
+                      exit="initial"
+                    >
+                      {plan.yearlyFreeMonths} month
+                      {plan.yearlyFreeMonths === 1 ? "" : "s"} free
+                    </motion.p>
+                  )}
+                </AnimatePresence>
+              </Card>
+            </li>
+          );
+        })}
       </ul>
 
       <div className="mt-6 flex h-10 items-center justify-center rounded-lg bg-neutral-200 p-2">
